4456ebf9b9dfb64c4a938b027c1c66a41a9552a6
Nhat Hoang <hoangnh092185@gmail.com>
2017-03-30 13:09:39 -0700

    test with hard code inputs.

--- tests/FoodSearch.test.js
+++ tests/FoodSearch.test.js
@@ -3,21 +3,96 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import FoodSearch from '../src/FoodSearch';
+import Client from '../src/Client';
+
+jest.mock('../src/Client');
 
 describe('FoodSearch', () => {
-  // ... initial state specs
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <FoodSearch />
+    );
+  });
+
+  afterEach(() => {
+    Client.search.mockClear();
+  });
+
+  it('should not display the remove icon', () => {
+    expect(
+      wrapper.find('.remove.icon').length
+    ).toBe(0);
+  });
+
+  it('should display zero rows', () => {
+    expect(
+      wrapper.find('tbody tr').length
+    ).toEqual(0);
+  });
 
   describe('user populates search field', () => {
+    const value = 'brocc';
+
     beforeEach(() => {
-      // ... simulate user typing "brocc" in input
+      const input = wrapper.find('input').first();
+      input.simulate('change', {
+        target: { value: value },
+      });
     });
 
-    // ... specs
+    it('should update state property `searchValue`', () => {
+      expect(
+        wrapper.state().searchValue
+      ).toEqual(value);
+    });
+
+    it('should display the remove icon', () => {
+      expect(
+        wrapper.find('.remove.icon').length
+      ).toBe(1);
+    });
+
+    // it('...todo...', () => {
+    //   const firstInvocation = Client.search.mock.calls[0];
+    //   console.log('First invocation:');
+    //   console.log(firstInvocation);
+    //   console.log('All invocations: ');
+    //   console.log(Client.search.mock.calls);
+    // });
+
+    it('should call `Client.searh() with value`', () => {
+      const invocationArgs = Client.search.mock.calls[0];
+      expect(
+        invocationArgs[0]
+      ).toEqual(value);
+    });
 
     describe('and API returns results', () => {
+      const foods = [
+        {
+          description: 'Broccolini',
+          kcal: '100',
+          protein_g: '11',
+          fat_g: '21',
+          carbohydrate_g: '31',
+        },
+        {
+          description: 'Broccoli rabe',
+          kcal: '200',
+          protein_g: '12',
+          fat_g: '22',
+          carbohydrate_g: '32',
+        },
+      ];
       beforeEach(() => {
-        // ... simulate API returning results
+        const invocationArgs = Client.search.mock.calls[0];
+        const cb = invocationArgs[1];
+        cb(foods);
+        wrapper.update();
       });
+
 
       // ... specs
 
